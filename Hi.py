f3efb059abac1c520bf0f1a65d8d067183a59048
GlebZhuravlev907 <65680656+GlebZhuravlev907@users.noreply.github.com>
2020-05-20 22:59:56 +0300

    Add files via upload

---
+++ Hi.py
@@ -0,0 +1,9 @@
+print("Nice to meet you, my name is Gleb, do you want to get to know me?");
+print("Type 1 - yes, or 2 if not");
+answer = int(input());
+if answer == 1:
+    print("Cool! I was born in Izhevsk, but now i live and study in Kazan. I'm 19 and i'm a student of Kazan Federal University. I love being there, in a big city with lots of new things to know about.Also i'm into gaming, different TV series and programming!Thank you!";
+elif answer == 2:
+    print("So sad :(, Good bye!");
+else:
+    print("u typed the wrong number, buddy");
